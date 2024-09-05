09d4af920418bdc6f7620aaf5b3802cbdd8a62dc
mariaureta <informariaureta@gmail.com>
2024-09-05 10:15:58 +0200

    Documentada la clase DTO
    Creada la documentación de la clase Categoría.

--- src/main/java/com/ipartek/modelo/dto/Categoria.java
+++ src/main/java/com/ipartek/modelo/dto/Categoria.java
@@ -1,9 +1,32 @@
 package com.ipartek.modelo.dto;
 
+/**
+ * <b> Clase Categoria </b>
+ * 
+ * <p> Clase Categoria que referencia a la tabla Categoria de la BBDD</p>
+ * @author María
+ *
+ */
+
 public class Categoria {
+	/**
+	 * Atributo ID:
+	 * <b> A LA HORA DE INSERTAR EN LA BD ES AUTONUMERICO, NO MODIFICAR</b>
+	 * 
+	 */
 	private int id;
+	
+	/**
+	 * Atributo nombre:
+	 * varchar(45) en la BD
+	 */
 	private String nombre;
 	
+	/**
+	 * Constructor completo 
+	 * @param id un integer, si no lo hemos recogido en la BD deberia ser un 0
+	 * @param nombre
+	 */
 	public Categoria(int id, String nombre) {
 		super();
 		this.id = id;
